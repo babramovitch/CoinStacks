85c36805b30da290a562ca07c54c4ce8f98d9234
Benjamin Abramovitch <benabramovitch@gmail.com>
2017-11-08 13:25:21 -0800

    Adding Fabric

--- app/src/main/java/com/nebulights/crytpotracker/MyApplication.kt
+++ app/src/main/java/com/nebulights/crytpotracker/MyApplication.kt
@@ -2,6 +2,8 @@ package com.nebulights.crytpotracker
 
 import android.app.Application
 import io.realm.Realm
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 /**
  * Created by babramovitch on 10/23/2017.
@@ -10,6 +12,7 @@ import io.realm.Realm
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        Fabric.with(this, Crashlytics())
         Realm.init(this)
     }
 }
